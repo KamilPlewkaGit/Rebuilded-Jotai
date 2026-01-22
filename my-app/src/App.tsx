23a6f7fafb2abdb067589798d4800aa52155d949
Kamil <kamilplewka12a@wp.pl>
2026-01-22 18:34:13 +0100

    change some code and now jotait is working

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,24 @@
-import './App.css';
-import { atom, useAtom } from "./jotai"; // poprawny import z "from"
+import { atom, useAtom, useAtomValue } from "./jotai";
 
-const salaryAtom = atom(100000);
+const salaryAtom = atom(100_000);
+const bonusAtom = atom(10_000);
+const totalSalaryAtom = atom((get) => get(salaryAtom) + get(bonusAtom));
 
 function App() {
   const [salary, setSalary] = useAtom(salaryAtom);
-
+  const [bonus, setBonus] = useAtom(bonusAtom);
+  const totalSalary = useAtomValue(totalSalaryAtom);
   return (
     <div>
       <div>
-        <input
-          type="number" // ważne dla liczb
-          value={salary}
-          onChange={(e) => setSalary(Number(e.target.value))} // konwersja na number
-        />
+        <input value={salary} onChange={(e) => setSalary(+e.target.value)} />
       </div>
+      <div>Salary:{salary}</div>
+      <div>
+        <input value={bonus} onChange={(e) => setBonus(+e.target.value)} />
+      </div>
+      <div>bonus:{bonus}</div>
+      <div>total salary :{totalSalary}</div>
     </div>
   );
 }
--- my-app/src/jotai.ts
+++ my-app/src/jotai.ts
@@ -1,25 +1,72 @@
-export function atom<AtomType>(initialValue: AtomType):{
-    get:()=>AtomType;
-    set:(newValue:AtomType)=>void;
-    subscribe:(callback:(newValue:AtomType)=>void)=>()=>void
-}{
-    let value=initialValue;
-    const subscribers= new Set<(newValue:AtomType)=>void>();
-    return{
-        get:()=>value,
-        set:(newValue)=>{
-            value=newValue 
-            subscribers.forEach((callback)=>callback(value))
-        },
-        subscribe:(callback)=>{
-            subscribers.add(callback);
-        
-        return ()=>{
-            subscribers.delete(callback);
-        }}
-    }
+import { useSyncExternalStore } from "react";
+
+interface Atom<AtomType> {
+  get: () => AtomType;
+  set: (newValue: AtomType) => void;
+  subscribe: (callback: (newValue: AtomType) => void) => () => void;
+}
+type AtomGetter<AtomType> = (
+  get: <Target>(a: Atom<Target>) => Target,
+) => AtomType;
+export function atom<AtomType>(
+  initialValue: AtomType | AtomGetter<AtomType>,
+): Atom<AtomType> {
+  let value =
+    typeof initialValue === "function" ? (null as AtomType) : initialValue;
+  let isDerived = typeof initialValue === "function";
+  let isInitialized = !isDerived;
+  const subscribers = new Set<(newValue: AtomType) => void>();
+  const dependencyUnsubscribers = new Set<() => void>();
+
+  function get<Target>(atom: Atom<Target>) {
+    let currentValue = atom.get();
+    const unsubscribe = atom.subscribe((newValue) => {
+      if (currentValue === newValue) return;
+      currentValue = newValue;
+      computeValue();
+      subscribers.forEach((callback) => callback(value));
+    });
+    dependencyUnsubscribers.add(unsubscribe);
+    return currentValue;
+  }
+  function computeValue() {
+    value =
+      typeof initialValue === "function"
+        ? (initialValue as AtomGetter<AtomType>)(get)
+        : value;
+  }
+  return {
+    get: () => {
+      if (!isInitialized) {
+        computeValue();
+        isInitialized = true;
+      }
+      return value;
+    },
+    set: (newValue) => {
+      value = newValue;
+      subscribers.forEach((callback) => callback(value));
+    },
+    subscribe: (callback) => {
+      if (!isInitialized && isDerived) {
+        computeValue();
+        isInitialized = true;
+      }
+      subscribers.add(callback);
+
+      return () => {
+        subscribers.delete(callback);
+      };
+    },
+  };
 }
 
-export function useAtom(){
-    
+export function useAtom<AtomType>(
+  atom: Atom<AtomType>,
+): [AtomType, (newValue: AtomType) => void] {
+  return [useSyncExternalStore(atom.subscribe, atom.get), atom.set];
+}
+
+export function useAtomValue<AtomType>(atom: Atom<AtomType>) {
+  return useSyncExternalStore(atom.subscribe, atom.get);
 }
